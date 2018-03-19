147dd035f8f833dd1f01b3d46be1ddbfead73484
medicily@hotmail.com <yulei92@tamu.edu>
2018-03-19 00:42:36 -0500

    First_Version_Chap_5

---
+++ Guess_Dotcom_HeadFirstJava/src/Dotcom.java
@@ -0,0 +1,11 @@
+public class Dotcom {
+    private int x_number = (int) (Math.random()*10);
+    private int y_number = (int) (Math.random()*10);
+    public int getX_number(){
+        return x_number;
+    }
+    public int getY_number(){
+        return y_number;
+    }
+
+}
---
+++ Guess_Dotcom_HeadFirstJava/src/GuessGame.java
@@ -0,0 +1,21 @@
+public class GuessGame {
+    Dotcom c1;
+    Dotcom c2;
+    Dotcom c3;
+
+    public void startGame(){
+        c1 = new Dotcom();
+        c2 = new Dotcom();
+        c3 = new Dotcom();
+        int guess_c1 = 0;
+        int guess_c2 = 0;
+        int guess_c3 = 0;
+        boolean c1_is_hit = false;
+        boolean c2_is_hit = false;
+        boolean c3_is_hit = false;
+        System.out.println("");
+
+    }
+
+
+}
