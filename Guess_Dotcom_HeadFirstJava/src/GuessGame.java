96c1d77a3bbce162f7e25aaf94e090c13c5cb25d
medicily@hotmail.com <yulei92@tamu.edu>
2018-03-19 16:25:40 -0500

    Add the function that display the number of hit com

--- Guess_Dotcom_HeadFirstJava/src/GuessGame.java
+++ Guess_Dotcom_HeadFirstJava/src/GuessGame.java
@@ -1,8 +1,5 @@
 public class GuessGame {
-    Dotcom c1;
-    Dotcom c2;
-    Dotcom c3;
-
+    int guess_num = 0;
     public void startGame() {
         Dotcom[] Com;
         Com = new Dotcom[3];
@@ -14,9 +11,13 @@ public class GuessGame {
         System.out.println("The game is started.");
         while (!(Com[0].get_hit() && Com[1].get_hit() && Com[2].get_hit())) {
             user_input = Helper.getUserInput("Enter a number");
-            Com[0].check_hit(user_input);
-            Com[1].check_hit(user_input);
-            Com[2].check_hit(user_input);
+            if (Com[0].check_hit(user_input))
+                    guess_num++;
+            if (Com[1].check_hit(user_input))
+                    guess_num++;
+            if (Com[2].check_hit(user_input))
+                    guess_num++;
+            System.out.println(guess_num+" of 3 com have been hit.");
         }
         System.out.println("The game is ended.");
     }
